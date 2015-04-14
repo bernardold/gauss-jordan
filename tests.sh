b1fa7194d4e4c1adc539d5ed44be342b81f07a2f
chris-asl <chrisasl92@gmail.com>
2015-04-14 22:05:31 +0300

    Update tests file (bigger test sizes)

--- tests.sh
+++ tests.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 log_all="all.csv"
 echo "proc_num, dimension, groups, time" >> ${log_all}
-for p in 2 4 8 16
+for groups in 1 0
 do
-	for k in 4 16 32 64
+	for k in 64 128 #256 512
 	do
-		let dimension=p*k
-		echo "Start test - proc_num[" ${p} "] and dimension [" ${dimension} "]"
-		for groups in 0 1
+		for p in 2 #4 8 16 32
 		do
+			let dimension=p*k
+			echo "Start test - proc_num[ ${p} ], dimension [ ${dimension} ], use k-groups[ ${groups} ] at `date +%d/%m-%R`"
 			echo -n "${p}, ${dimension}, ${groups}, ">> ${log_all}
 			echo `mpiexec -n ${p} ./gj ${dimension} ${groups}` >> ${log_all}
 		done
